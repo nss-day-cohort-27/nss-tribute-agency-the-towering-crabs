d1c1842ac965cf5624a36c2fdf68c304c0ea6239
Kelly Cook <kellycook@Kellys-MacBook-Pro.local>
2018-07-18 11:24:28 -0500

    first commit

---
+++ html/js/main.js
@@ -0,0 +1,23 @@
+const fragment = document.createDocumentFragment()
+const main = document.querySelector("#main-article")
+
+const title = document.createElement("header")
+title.classList = "title"
+title.textContent = "Andy Warhol"
+
+const collab = document.createElement("ul")
+collab.textContent = "Collaborators:"
+collab.classList = "collab-list"
+
+const collabListItem1 = document.createElement("li")
+collabListItem1.classList = "collab-list-items"
+collabListItem1.textContent = "The Velvet Underground"
+
+
+
+collab.appendChild(collabListItem1)
+
+title.appendChild(collab)
+fragment.appendChild(title)
+
+main.appendChild(fragment)
